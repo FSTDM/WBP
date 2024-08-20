d1156deb703bd8146880f3b1e306a15f2b1cde0f
FSTDM <FSTDM@GMX.COM>
2024-08-20 15:32:51 -0700

    Update autoexec.py

--- autoexec.py
+++ autoexec.py
@@ -1,1 +1,20 @@
-print("This is autoexec.py")
+class dotdict(dict):
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+class FSTDM:
+    def INIT():
+        FSTDM.Info = dotdict()
+        Info = FSTDM.Info
+        Info.MachineId = machineid.id()
+        Info.OsName = platform.system()
+        Info.OsRelease = platform.release()
+        Info.PythonVersion = platform.python_version()
+        Info.PythonMagicNumber = int.from_bytes(importlib.util.MAGIC_NUMBER[:2],"little")
+        Info.PathSep = importlib._bootstrap_external.path_separators
+        Info.CurrentPath = str(pathlib.Path().absolute())
+        Info.UserPath = str(pathlib.Path.home())
+FSTDM.INIT()
+
+print(FSTDM.Info)
+
